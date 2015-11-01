f603249ff95ce5a818799fd5ed6b3791514f915f
Agoston <agoston@localhost.localdomain>
2015-10-31 22:21:33 -0400

    Added a rectangle to serve as the player. It can move left and right, and the camera does not currently track it, and gravity does not work either yet. I also split the game up into multiple files to make it more manageable and cleaner.

---
+++ js/entity.js
@@ -0,0 +1,103 @@
+/* jshint devel: true */
+/* jshint browser: true */
+
+function AnimationFrame() {
+	this.positionX = 0;
+	this.positionY = 0;
+	this.width = 0;
+	this.height = 0;
+}
+
+function Entity() {
+	this.positionX = 0.0;
+	this.positionY = 0.0;
+	this.dx = 0.0;
+	this.dy = 0.0;
+	this.acceleration = 0.0;
+	this.maxVelocity = 0.0;
+	this.tileMap = null;
+	this.animationPositions = [];
+	this.height = 0;
+	this.width = 0;
+	this.gravity = 9.81;
+	this.texture = null;
+	this.xBounds = 0;
+	this.yBounds = 0;
+}
+
+Entity.prototype.setPosition = function(x, y) {
+	this.positionX = x;
+	this.positionY = y;
+};
+
+Entity.prototype.getX = function() {
+	return this.positionX;
+};
+
+Entity.prototype.getY = function() {
+	return this.positionY;
+};
+
+Entity.prototype.getHeight = function() {
+	return this.height;
+};
+
+Entity.prototype.getWidth = function() {
+	return this.width;
+};
+
+Entity.prototype.setBounds = function(xBound, yBound) {
+	this.xBounds = xBound;
+	this.yBounds = yBound;
+};
+
+Player.prototype = new Entity();
+
+function Player() {
+	this.texture = Game.res.getImage('player');
+	this.width = this.texture.width;
+	this.height = this.texture.height;
+	this.acceleration = 0.025;
+	this.maxVelocity = 0.075;
+}
+
+Player.prototype.update = function() {	
+	if(Key.isDown(Key.D)) {
+		this.dx += this.acceleration;
+		if(this.dx > this.maxVelocity) {
+			this.dx = this.maxVelocity;
+		}
+	}
+	
+	else if(Key.isDown(Key.A)) {
+		this.dx -= this.acceleration;
+		if(this.dx < -this.maxVelocity) {
+			this.dx = -this.maxVelocity;
+		}
+	}
+	
+	else {
+		this.dx = 0;
+	}
+	
+	this.positionX += this.dx;
+	
+	if(this.positionX < 0) {
+		this.positionX = 0;
+	}
+	
+	if(this.positionX > this.xBounds) {
+		this.positionX = this.xBounds - this.width;
+	}
+};
+
+Player.prototype.draw = function(camera) {
+	camera.draw(this.texture, this.positionX, this.positionY, this.width,
+			   	this.height, 0, 0);
+	ctx.fillStyle = "white";
+		ctx.fillText('Player dx is ' + this.dx, 0, 10);
+//	ctx.beginPath();
+//	ctx.drawImage(this.texture, 0, 0, this.width, this.height, this.positionX,
+//				  this.positionY, this.width, this.height);
+//	ctx.closePath();
+};
--- js/game.js
+++ js/game.js
@@ -1,522 +1,6 @@
 /* jshint devel: true */
 /* jshint browser: true */
 
-var c = document.getElementById('c');
-var ctx = c.getContext('2d');
-var FPS = 60;
-
-// dimensions of canvas (16:9)
-var canvasWidth = 1024;
-var canvasHeight = 576;
-
-c.width = canvasWidth;
-c.height = canvasHeight;
-
-var clear = function(strColor) {
-    'use strict';
-    ctx.fillStyle = strColor;
-    // clear the whole surface
-    ctx.beginPath();
-    ctx.rect(0, 0, canvasWidth, canvasHeight);
-    ctx.closePath();
-    ctx.fill();
-};
-
-// Creates a numeric hash of a string
-// taken from
-//http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-var hashString = function(string) {
-	'use strict';
-	var hash = 0;
-	if(string.length === 0) {
-		return hash;
-	}
-	for(var i = 0; i < string.length; ++i) {
-		var char = string.charCodeAt(i);
-		hash = ((hash << 5) - hash) + char;
-		// convert to 32-bit integer
-		hash &= hash;
-	}
-	
-	return hash;
-};
-
-// GameStateManager - manages the game states. Uses a stack to do this.
-function GameStateManager() {
-    'use strict';
-    this.states = [];
-	this.tempState = null;
-	this.changingState = false;
-	this.poppingState = false;
-	this.pushingState = false;
-}
-
-// Adds a state to the stack.
-GameStateManager.prototype.pushState = function(state) {
-    'use strict';
-	this.tempState = state;
-	this.pushingState = true;
-};
-
-// Removes the state at the top of the stack.
-GameStateManager.prototype.popState = function() {
-    'use strict';
-	this.poppingState = true;
-};
-
-GameStateManager.prototype.changeState = function(state) {
-	this.changingState = true;
-	this.tempState = state;
-};
-
-// Updates the current state.
-GameStateManager.prototype.update = function() {
-    'use strict';
-	
-	if(this.changingState) {
-		this.changingState = false;
-		this.states.pop();
-		this.states.push(this.tempState);
-	}
-	
-	if(this.pushingState) {
-		this.pushingState = false;
-		this.states.push(this.tempState);
-	}
-	
-	if(this.poppingState) {
-		this.poppingState = false;
-		this.states.pop();
-	}
-	
-    this.states[this.states.length - 1].update();
-};
-
-// Draws the current state
-GameStateManager.prototype.draw = function() {
-    'use strict';
-    this.states[this.states.length - 1].draw();
-};
-
-// Key - manages keyboard inputs
-var Key = {
-    pressed: [],
-    prevKeys: [],
-
-    // WASD
-    UP: 38,
-    LEFT: 37,
-    DOWN: 40,
-    RIGHT: 39,
-    SPACE: 32,
-	ENTER: 13,
-	W: 87,
-	A: 65,
-	S: 83,
-	D: 68,
-
-	/// Returns true as long as the key is held down(fires continously).
-    isDown: function(keyCode) {
-        'use strict';
-        return this.pressed[keyCode];
-    },
-
-    onKeyDown: function(event) {
-        'use strict';
-        this.pressed[event.keyCode] = true;
-    },
-
-    onKeyUp: function(event) {
-        'use strict';
-        this.pressed[event.keyCode] = false;
-    },
-    
-	/// Backs up the key states
-    backupKeys: function() {
-        'use strict';
-		var i;
-        for (i = 0; i < this.pressed.length; ++i) {
-			this.prevKeys[i] = this.pressed[i];
-		}
-    },
-	
-	/// Allows the keyboard to fire once instead of firing continously.
-	isKeyPressed: function(keyCode) {
-		'use strict';
-		return !this.prevKeys[keyCode] && this.pressed[keyCode];
-	}
-};
-
-function Resource(resId, res) {
-	'use strict';
-	this.id = hashString(resId);
-	this.resource = res;
-}
-
-// Keeps track of resources. Each resource is stored with a key.
-function ResourceManager() {
-	'use strict';
-	this.images = [];
-}
-
-ResourceManager.prototype.addImage = function(key, resPath) {
-	var res = new Image();
-	res.src = resPath;
-	this.images.push(new Resource(key, res));
-};
-
-ResourceManager.prototype.getImage = function(key) {
-	var hashedKey = hashString(key);
-	for(var i = 0; i < this.images.length; ++i) {
-		
-		if(hashedKey === this.images[i].id) {
-			return this.images[i].resource;
-		}
-	}
-};
-
-
-var Game = { };
-
-Game.update = function(numTicks) {
-    'use strict';
-    var i;
-    for (i = 0; i < numTicks; ++i) {
-        Game.gsm.update();
-		Key.backupKeys();
-    }
-};
-
-Game.draw = function() {
-    'use strict';
-    Game.gsm.draw();
-};
-
-function setInitialState() {
-    'use strict';
-    Game.gsm = new GameStateManager();
-    Game.lastTick = window.performance.now();
-    Game.tickLength = 1000 / FPS;
-    Game.lastRender = Game.lastTick;
-	Game.res = new ResourceManager();
-	Game.levelLoaded = false;
-
-    window.addEventListener('keyup', function (event) { Key.onKeyUp(event); }, false);
-    window.addEventListener('keydown', function (event) { Key.onKeyDown(event); }, false);
-}
-
-// The camera that follows the player around. It helps draw objects in the
-// proper place. It's origin is in the upper left corner.
-// camX - starting x coordinate
-// camY - starting y coordinate
-// camWidth - the width of the camera viewport
-// camHeight - the height of the camera viewport
-function Camera(camX, camY, camWidth, camHeight) {
-	this.positionX = camX;
-	this.positionY = camY;
-	this.xBounds = 0;
-	this.yBounds = 0;
-	this.width = camWidth;
-	this.height = camHeight;
-}
-
-// Sets the boundaries of the camera. It cannot go past these bounds. It is
-// assumed that (0, 0) is the boundary from the left side.
-Camera.prototype.setBounds = function(camXBounds, camYBounds) {
-	this.xBounds = camXBounds;
-	this.yBounds = camYBounds;
-};
-
-Camera.prototype.setHeight = function(height) {
-	this.height = height;
-};
-
-Camera.prototype.setWidth = function(width) {
-	this.width = width;
-};
-
-// Draws the image on the screen. The images that show up are the ones that
-// are supposed to be in the camera viewing port.
-// img - the image to draw
-// x - the actual x position of the object in the game world
-// y - the actual y position of the object in the game world
-// width - the width of the object
-// height - the height of the object
-// sourceX - the x location of the object on the image
-// sourceY - the y location of the object on the image
-Camera.prototype.draw = function(img, x, y, width, height, sourceX, sourceY) {
-	ctx.beginPath();
-	ctx.drawImage(img, sourceX, sourceY, width, height, x - this.positionX, 
-				  y - this.positionY, width, height);
-	ctx.closePath();
-};
-
-// Only use this function to set x & y so that it's easier to see where the
-// position is set.
-Camera.prototype.setPosition = function(x, y) {
-	this.positionX = x;
-	this.positionY = y;
-};
-
-Camera.prototype.checkBounds = function() {
-	// if it goes past right border of map, put it to the edge
-	if(this.positionX + canvasWidth > this.xBounds) {
-		this.positionX = this.xBounds - canvasWidth;
-	}
-	// if it goes past the left border of map, put it to the edge
-	if(this.positionX < 0) {
-		this.positionX = 0;
-	}
-	
-	// if it goes past the bottom border of map, put it to the edge
-	if(this.positionY + canvasWidth > this.yBounds) {
-		this.positionY = this.yBounds - canvasHeight;
-	}
-	// if it goes past the upper border of map, put it to the edge
-	if(this.positionY < 0) {
-		this.positionY = 0;
-	}
-};
-
-function AnimationFrame() {
-	this.positionX = 0;
-	this.positionY = 0;
-	this.width = 0;
-	this.height = 0;
-}
-
-function Entity() {
-	this.positionX = 0;
-	this.positionY = 0;
-	this.dx = 0;
-	this.dy = 0;
-	this.acceleration = 0;
-	this.maxVelocity = 0;
-	this.tileMap = null;
-	this.animationPositions = [];
-	this.height = 0;
-	this.width = 0;
-	this.gravity = 9;
-	this.texture = null;
-}
-
-Entity.prototype.setPosition = function(x, y) {
-	this.positionX = x;
-	this.positionY = y;
-};
-
-Entity.prototype.getX = function() {
-	return this.positionX;
-};
-
-Entity.prototype.getY = function() {
-	return this.positionY;
-};
-
-Entity.prototype.getHeight = function() {
-	return this.height;
-};
-
-Entity.prototype.getWidth = function() {
-	return this.width;
-};
-
-function Player(tileMap) {
-	Entity.call();
-	
-	this.positionX = 30;
-	
-}
-
-Player.prototype = new Entity();
-
-function Tile(tileWidth, tileHeight, tileType, tileImgXPosition, tileImgYPosition) {
-	this.width = tileWidth;
-	this.height = tileHeight;
-	this.type = tileType;
-	this.imgXPosition = tileImgXPosition;
-	this.imgYPosition = tileImgYPosition;
-}
-
-// tile types
-Tile.UNBLOCKED = 0;
-Tile.BLOCKED = 1;
-
-// TileMapLayer - A layer composed of tiles for the TileMap.
-// layerName - name of the layer
-// mapWidth - width of map in tiles
-// mapHeight - height of map in tiles
-// layerName - the name of the layer
-// tileSetWidth - the number of tiles across the tileset
-// tileSetHeight - the number of tiles high the tileset is
-// tileData - the arrangement of tiles in xml format
-function TileMapLayer(mapWidth, mapHeight, tileWidth, tileHeight, layerName, 
-					   tileSetWidth, tileSetHeight, tileCount, tileData) {
-	'use strict';
-	this.name = layerName;
-	this.width = mapWidth;
-	this.height = mapHeight;
-	
-	this.tiles = [];
-	this.data = tileData;
-	this.tileHeight = tileHeight;
-	this.tileWidth = tileWidth;
-	
-	// The tileset that makes up the tilemap
-	this.tileSet = Game.res.getImage('tileSet');
-	
-	// An array of integers used to figure out whether there is a tile where the
-	// player is
-	this.map = [];
-	
-	// copy data into the map
-	for(var count = 0; count < tileData.length; ++count) {
-		this.map.push(tileData[count]);
-	}
-	
-	// initialize tile types
-	for(var j = 0; j < tileSetHeight / tileHeight; ++j) {
-		for(var i = 0; i < tileSetWidth / tileWidth; ++i) {
-			this.tiles.push(new Tile(this.tileWidth, this.tileHeight,
-									Tile.BLOCKED, i * this.tileWidth,
-									j * this.tileHeight));
-		}
-	}
-	
-	// add a blank tile to the beginning because 0 means no tile is drawn
-	this.tiles.splice(0, 0, new Tile(this.tileWidth, this.tileHeight, Tile.UNBLOCKED, 0, 0));	
-	
-}
-
-TileMapLayer.prototype.getTile = function(x, y) {
-	return this.tiles[this.map[y / this.tileHeight][x / this.tileWidth]];
-};
-
-TileMapLayer.prototype.draw = function(camera) {
-	// where to start drawing the tiles. Draw 1 more on each side to cover up
-	// all empty places
-	var startX = (camera.positionX / this.tileWidth - 1);
-	
-	if(startX < 0) startX = 0;
-	
-	
-	var endX = ((camera.positionX + camera.width) / this.tileWidth + 1);
-	if(endX > this.width) endX = this.width;
-
-	var startY = (camera.positionY / this.tileHeight - 1);
-	if(startY < 0) startY = 0;
-	
-	var endY = ((camera.positionY + camera.height) / this.tileHeight + 1);
-	
-	ctx.beginPath();
-	for(var row = 0; row < this.height; ++row) {
-		for(var col = 0; col < this.width; ++col) {
-			var tileId = this.map[this.width * row + col];
-			if(tileId !== 0) {
-				var t = this.tiles[tileId];
-				camera.draw(this.tileSet, col * this.tileWidth, row * this.tileHeight,
-					   this.tileWidth, this.tileHeight, t.imgXPosition, t.imgYPosition);
-			}
-		}
-	}
-	
-	ctx.closePath();
-};
-
-// A TileMapObject can be anything on the tilemap. For example, it could be
-// coins, or enemies.
-function TileMapObject(positionX, positionY, objId) {
-	'use strict';
-	this.positionX = positionX;
-	this.positionY = positionY;
-	this.id = objId;
-}
-
-function TileMapObjectLayer(layerName) {
-	'use strict';
-	this.name = layerName;
-	this.objects = [];
-	
-}
-
-TileMapObjectLayer.prototype.addObject = function(tileObj) {
-	this.objects.push(tileObj);
-};
-
-function TileMap() {
-	'use strict';
-	this.mapLayers = [];
-	this.objectLayers = [];
-}
-
-TileMap.prototype.loadFile = function(pathToFile, callback) {
-	'use strict';
-	var xhr = new XMLHttpRequest();
-	var that = this;
-	xhr.onreadystatechange = function() {
-		if(xhr.readyState === 4 && xhr.status === 200) {
-			console.log('reading file in');
-			
-			var levelData = JSON.parse(xhr.responseText);
-			
-			var lvlHeight = levelData.height;
-			var lvlWidth = levelData.width;
-			
-			// tilemap tile data
-			var tilesets = levelData.tilesets;
-			var tilesetWidth = tilesets[0].imagewidth;
-			var tilesetHeight = tilesets[0].imageheight;
-			var tileWidth = levelData.tilesets[0].tilewidth;
-			var tileHeight = levelData.tilesets[0].tileheight;
-			var tileCount = levelData.tilesets[0].tilecount;
-			
-			Game.res.addImage('tileSet', '/rise-of-oxshan/res/maps/' + tilesets[0].image);
-			
-			var layers = levelData.layers;
-			
-			for(var i = 0; i < layers.length; ++i) {
-				// check whether the layer is a tile layer or object layer
-				if(layers[i].type === 'tilelayer') {
-					that.mapLayers.push(new TileMapLayer(lvlWidth, lvlHeight,
-														tileWidth, tileHeight,
-														layers[i].name, tilesetWidth,
-														tilesetHeight, 
-														 tileCount, layers[i].data));
-				}
-				else if(layers[i].type === 'objectgroup') {
-					var layerName = layers[i].name;
-					var objects = layers[i].objects;
-					that.objectLayers.push(new TileMapObjectLayer(layerName));
-					for(var obj = 0; obj < objects.length; ++obj) {
-						// get object's x and y position, and it's id
-						that.objectLayers[that.objectLayers.length - 1].addObject(
-														new TileMapObject(
-														objects[obj].x,
-														objects[obj].y,
-														objects[obj].id));
-					}
-				}
-			}
-			
-			Game.levelLoaded = true;
-			
-			console.log('finished parsing tilemap. calling callback');
-			
-			callback();
-			
-		}
-	};
-	xhr.overrideMimeType("application/json");
-	xhr.open('GET', pathToFile, true);
-	xhr.send(null);
-};
-
-TileMap.prototype.draw = function(camera) {
-	for(var i = 0, j = this.mapLayers.length; i < j; ++i) {
-		this.mapLayers[i].draw(camera);
-	}
-};
-
 // GameState - base class for each state of the game (menu, playing, etc)
 function GameState() {'use strict'; }
 
@@ -536,13 +20,12 @@ function MenuState() {
 	this.selectedOption = 0;
 	this.selectorImgHeight = 12;
 	this.selectorImgWidth = 12;
-	this.selectorImg = new Image(this.selectorImgWidth, this.selectorImgHeight);
-	this.selectorImg.src = '/rise-of-oxshan/res/grey_dot.png';
+	console.log(Game.res.getImage('grey_dot'));
+	this.selectorImg = Game.res.getImage('grey_dot');
 	
 	this.textWidth = 40;
 	this.textHeight = 21;
-	this.menuTextImg = new Image(this.textWidth, this.textHeight);
-	this.menuTextImg.src = '/rise-of-oxshan/res/menu-text.png';
+	this.menuTextImg = Game.res.getImage('menu_text');
 }
 
 function PlayState() {
@@ -554,14 +37,18 @@ function PlayState() {
 	this.camera.setHeight(canvasHeight);
 	this.camera.setWidth(canvasWidth);
 	
-	this.tileMap = new TileMap();
+	var that = this;
 	
 	this.tileMap = new TileMap();
-	var that = this;
+	this.player = new Player();
+	
+	this.tileMap = new TileMap();
+	
 	this.tileMap.loadFile('/rise-of-oxshan/res/maps/level-1.json', function() {
 		that.continueLoadingLevel();
 	});
-		
+	
+	
 }
 
 
@@ -625,12 +112,30 @@ PlayState.prototype = new GameState();
 
 PlayState.prototype.continueLoadingLevel = function() {
 	console.log('continue loading level');
+	
+	// setup camera bounds and position
 	this.camera.setBounds(this.tileMap.mapLayers[0].width * 
 						  this.tileMap.mapLayers[0].tileWidth,
 						  this.tileMap.mapLayers[0].height *
 						 this.tileMap.mapLayers[0].tileHeight);
 	this.camera.setPosition(0, this.tileMap.mapLayers[0].height * 
-							this.tileMap.mapLayers[0].tileHeight - this.tileMap.mapLayers[0].height);
+							this.tileMap.mapLayers[0].tileHeight - canvasHeight);
+	
+	console.log('setup player');
+	this.player.setBounds(this.tileMap.mapLayers[0].width * 
+						  this.tileMap.mapLayers[0].tileWidth,
+						  this.tileMap.mapLayers[0].height * 
+						  this.tileMap.mapLayers[0].tileHeight);
+	this.player.setPosition(this.tileMap.getLayer('player').objects[0].positionX,
+							this.tileMap.getLayer('player').objects[0].positionY);
+//							704);
+	console.log(this.tileMap.getLayer('player').objects[0].positionY);
+	console.log(this.camera.positionY);
+	
+//	console.log(this.tileMap.mapLayers[0].height * 
+//							this.tileMap.mapLayers[0].tileHeight);
+//	console.log(this.tileMap.mapLayers[0].height);
+	
 };
 
 PlayState.prototype.update = function () {
@@ -641,7 +146,8 @@ PlayState.prototype.update = function () {
     }
 	
 	if(Game.levelLoaded) {
-			this.camera.checkBounds();
+		this.player.update(this.camera);
+		this.camera.checkBounds();
 	}
 	
 };
@@ -650,12 +156,67 @@ PlayState.prototype.draw = function () {
     'use strict';
     clear('#655541');
 	this.tileMap.draw(this.camera);
+	this.player.draw(this.camera);
 };
 
 
-// start the game
-(function () {
+window.onload = function() {
+	var sources = {
+		grey_dot: '/rise-of-oxshan/res/grey_dot.png',
+		menu_text: '/rise-of-oxshan/res/menu-text.png',
+		player: '/rise-of-oxshan/res/player.png'
+	};
+	
+	loadImages(sources, startGame);
+};
+
+function loadImages(imgSources, callback) {
+	var images = {};
+	var loadedImages = 0;
+	var numImages = 0;
+	
+	for(var src in imgSources) {
+		numImages++;
+	}
+	
+	for(src in imgSources) {
+		images[src] = new Image();
+		images[src].src = imgSources[src];
+		images[src].onload = function() {
+			if(++loadedImages >= numImages) {
+				console.log('images loaded');
+				callback(images);
+			}
+		};
+	}
+}
+
+function storeImages(images) {
+	for(var src in images) {
+		console.log(images[src]);
+		Game.res.addImage(src, images[src]);
+	}
+}
+
+function setInitialState() {
     'use strict';
+    Game.gsm = new GameStateManager();
+    Game.lastTick = window.performance.now();
+    Game.tickLength = 1000 / FPS;
+    Game.lastRender = Game.lastTick;
+	Game.res = new ResourceManager();
+	Game.levelLoaded = false;
+	Game.time = 0;
+	Game.numFrames = 0;
+
+    window.addEventListener('keyup', function (event) { Key.onKeyUp(event); }, false);
+    window.addEventListener('keydown', function (event) { Key.onKeyDown(event); }, false);
+}
+
+// start the game
+function startGame(images) {
+    'use strict';
+	
     function main(tFrame) {
         Game.stopMain = window.requestAnimationFrame(main);
         var nextTick = Game.lastTick + Game.tickLength,
@@ -672,6 +233,8 @@ PlayState.prototype.draw = function () {
     }
 
     setInitialState();
+	storeImages(images);
+	
     Game.gsm.pushState(new MenuState());
     main(window.performance.now());
-})();
+}
